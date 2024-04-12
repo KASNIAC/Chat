1c836fa607d4f688537abea05fd11f8bbe8aa0ba
KASNIAC <kasniac10@gmail.com>
2024-04-12 14:41:26 -0600

    Borrando todo

--- version01-index.php_vulnerabilidades/backend/_pruebas.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $conexion = new mysqli('127.0.0.1:3307', 'root', 'root', 'test');
-
-   // $nombre = "wendy's";
-   $nombre = $conexion->escape_string("Wendy's");
-   $conexion->query("INSERT INTO persona (nombre) VALUES ('$nombre')");
-?>
