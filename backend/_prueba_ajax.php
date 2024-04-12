790e3a8fa70af191ebd1572d15371681600652e1
Rodrigo Castro <rcc.dark@gmail.com>
2024-04-11 19:16:46 -0600

    Update _prueba_ajax.php

--- backend/_prueba_ajax.php
+++ backend/_prueba_ajax.php
@@ -1,3 +1,14 @@
 <?php
-   echo 'PERRO LOCO';
+   $persona = [
+      'nombre' => 'Alexis',
+      'edad' => 24,
+      'hobby' => 'Ser el mejor',
+      'mascotas' => [
+         'perrito', 'gatito', 'cerdito'
+      ]
+   ];
+
+   echo "hola mundo\n";
+   echo json_encode($persona), "\n";
+   echo "adios mundo\n";
 ?>
