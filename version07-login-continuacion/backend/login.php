03be7d3b1bf0bbaaee2166091352cf3b24a1443b
Rodrigo Castro <rcc.dark@gmail.com>
2024-04-19 19:57:54 -0600

    continuando con el login

--- version07-login-continuacion/backend/login.php
+++ version07-login-continuacion/backend/login.php
@@ -3,37 +3,19 @@
    require_once('secret/config.php');
    
    session_name(APP_NAME);
+   session_start();
+   
    if (isset($_POST['comprobante'])) {
       $cliente = new Google_Client([ 'client_id' => '330917364178-utl74obou3uhk4ttk86tu0jlbnod1dev.apps.googleusercontent.com' ]);
       $cliente->setHttpClient(new \GuzzleHttp\Client([ 'curl' => [ CURLOPT_SSL_VERIFYPEER => false ] ])); // CURLOPT_SSL_VERIFYPEER => false : potencialmente es una mala practica; en caso de hackeo de red o de que hackeen a google habría problemas.
       $resultado = $cliente->verifyIdToken($_POST['comprobante']);
+      
       if ($resultado == false) {
          die(json_encode('error-comprobante'));
       } else {
-         session_start();
-         $_SESSION['logeado'] = true;
-
          $conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
          $nombre = $conexion->escape_string($resultado['name']);
          $email = $conexion->escape_string($resultado['email']);
-   
-         /*
-         // Solo lo agrego si no existe
-         $existe = $conexion->query("SELECT email FROM Usuario WHERE email = '$email'")->fetch_all(MYSQLI_ASSOC);
-         if(count($existe) == 0){
-            $conexion->query("INSERT INTO Usuario (nombre, email) VALUES ('$nombre', '$email')");
-         }
-         */
-         
-         /* No regañar si ya hay email duplicado
-         $conexion->query("
-            INSERT IGNORE INTO 
-               Usuario (nombre, email) 
-            VALUES 
-               ('$nombre', '$email')
-         ");
-         */
-         
          $conexion->query("
             INSERT INTO 
                Usuario (nombre, email) 
@@ -42,10 +24,18 @@
             ON DUPLICATE KEY UPDATE
                nombre = '$nombre'
          ");
-         die(json_encode([ 'email' => $resultado['email'], 'nombre' => $resultado['name']]));
-         //die("{$resultado['sub']} {$resultado['email']} {$resultado['name']}"); // Puedo mandar a veces JSON y a veces cadenas?
+         
+         $_SESSION['logeado'] = true;
+         $_SESSION['nombre'] = $resultado['name'];
+         $_SESSION['email'] = $resultado['email'];
+         
+         die(json_encode([ 'email' => $_SESSION['email'], 'nombre' => $_SESSION['nombre'] ]));
       }
    } else {
-      die(json_encode('sin-comprobante'));
+      if (empty($_SESSION['logeado'])) {
+         die(json_encode('no-autenticado'));
+      } else {
+         die(json_encode([ 'email' => $_SESSION['email'], 'nombre' => $_SESSION['nombre'] ]));
+      }
    }
 ?>
